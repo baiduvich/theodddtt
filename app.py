33e77b08470a8667f9915953a49cb399e9e8385c
Marouane <baid.marouane1@gmail.com>
2023-09-01 10:59:11 +0100

    7th edit

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import tempfile
 from threading import Timer
 from waitress import serve
 
-def delete_file_after_delay(delay, file_path):
-    Timer(delay, os.remove, [file_path]).start()
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -26,42 +23,47 @@ class Convert(Resource):
             return {'error': 'invalid format'}, 400
 
         try:
-            # Create a temporary directory using tempfile
             with tempfile.TemporaryDirectory() as tmpdirname:
                 if not os.path.exists(tmpdirname):
                     return {'error': 'Temp directories were not created!'}, 500
 
                 unique_filename = secure_filename(file.filename)
-
                 input_file_path = os.path.join(tmpdirname, f'input-{unique_filename}.odt')
                 output_file_path = os.path.join(tmpdirname, f'output-{unique_filename}.{format}')
-
+                
+                # Log to debug input_file_path and output_file_path
+                print(f"Debug: Input file path is {input_file_path}")
+                print(f"Debug: Output file path is {output_file_path}")
+                
                 file.save(input_file_path)
+                
+                libreoffice_command = [
+                    'libreoffice',
+                    '--headless',
+                    '--convert-to',
+                    format,
+                    '--outdir',
+                    tmpdirname,
+                    input_file_path
+                ]
+
+                print(f"Running command: {' '.join(libreoffice_command)}")
 
                 try:
-                    subprocess.run([
-                        'libreoffice',
-                        '--headless',
-                        '--convert-to',
-                        format,
-                        '--outdir',
-                        tmpdirname,
-                        input_file_path
-                    ], check=True)
+                    subprocess.run(libreoffice_command, check=True)
                 except subprocess.CalledProcessError:
                     return {'error': 'conversion failed'}, 500
 
-                # Check if the output file was successfully created
                 if not os.path.exists(output_file_path):
                     return {'error': 'output file was not created'}, 500
 
-                # Send the converted file directly to the user
                 return send_file(
                     output_file_path,
                     as_attachment=True,
                     download_name=f'converted-{unique_filename}.{format}'
                 )
         except Exception as e:
+            print(f"An exception occurred: {e}")
             return {'error': str(e)}, 500
 
 api.add_resource(Convert, '/convert/<string:format>')
