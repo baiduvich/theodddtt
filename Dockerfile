8e08160fd6310144013170f36d03a83cfcf099dd
Marouane <baid.marouane1@gmail.com>
2023-09-01 10:28:51 +0100

    5th edit

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # Use an official Python runtime as base image
 FROM python:3.8-slim-buster
 
-# Install LibreOffice and Java Runtime Environment
+# Install LibreOffice, Java Runtime Environment, and PostgreSQL client
 RUN apt-get update && \
-    apt-get install -y --no-install-recommends libreoffice default-jre libreoffice-java-common && \
+    apt-get install -y --no-install-recommends libreoffice default-jre libreoffice-java-common postgresql-client && \
     apt-get clean
 
 # Set the working directory in the container
@@ -19,7 +19,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app
 
 # Make port 5000 available to the world outside this container
-EXPOSE 5000
+EXPOSE 8080
 
 # Run app.py when the container launches
-CMD ["waitress-serve", "--host=0.0.0.0", "--port=5000", "app:app"]
+CMD ["waitress-serve", "--host=0.0.0.0", "--port=8080", "app:app"]
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, send_file, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import subprocess
 import os
@@ -7,12 +8,23 @@ import tempfile
 from threading import Timer
 from waitress import serve
 
-def delete_file_after_delay(delay, file_path):
-    Timer(delay, os.remove, [file_path]).start()
-
+# Initialize Flask and SQLAlchemy
 app = Flask(__name__)
+# Configure database URI (Replace these with your PostgreSQL credentials)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://odtdatabase_user:PAELaEqjZAdPgGJoaMsB2IjiNRW9qQGq@dpg-cjoqppr6fquc73fgtlo0-a/odtdatabase'
+db = SQLAlchemy(app)
 api = Api(app)
 
+# Define SQLAlchemy model for files
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary)
+
+# Function to delete file blob from database after delay
+def delete_blob_after_delay(delay, blob_id):
+    Timer(delay, lambda: db.session.query(File).filter(File.id == blob_id).delete()).start()
+
+# Convert class (Resource)
 class Convert(Resource):
     def post(self, format):
         if 'file' not in request.files:
@@ -25,14 +37,20 @@ class Convert(Resource):
         if format not in ['docx', 'pdf', 'txt']:
             return {'error': 'invalid format'}, 400
 
-        # Create a temporary directory using tempfile
+        # Step 1: Save the file blob to the database
+        file_data = File(data=file.read())
+        db.session.add(file_data)
+        db.session.commit()
+
+        # Step 2 and 3: Save the blob to a temp file and convert it
         with tempfile.TemporaryDirectory() as tmpdirname:
             unique_filename = secure_filename(file.filename)
 
             input_file_path = os.path.join(tmpdirname, f'input-{unique_filename}.odt')
             output_file_path = os.path.join(tmpdirname, f'output-{unique_filename}.{format}')
 
-            file.save(input_file_path)
+            with open(input_file_path, 'wb') as f:
+                f.write(file_data.data)
 
             try:
                 subprocess.run([
@@ -47,14 +65,22 @@ class Convert(Resource):
             except subprocess.CalledProcessError:
                 return {'error': 'conversion failed'}, 500
 
-            # Send the converted file directly to the user
-            return send_file(
-                output_file_path,
-                as_attachment=True,
-                download_name=f'converted-{unique_filename}.{format}'
-            )
+            # Step 4: Read the converted file and save back to the database
+            with open(output_file_path, 'rb') as f:
+                converted_data = File(data=f.read())
+                db.session.add(converted_data)
+                db.session.commit()
 
+            # Delete blobs from database after 30 seconds
+            delete_blob_after_delay(30, file_data.id)
+            delete_blob_after_delay(30, converted_data.id)
+
+        return jsonify({"message": "File conversion successful!", "converted_file_id": converted_data.id})
+
+# Add the Convert resource to the API
 api.add_resource(Convert, '/convert/<string:format>')
 
+# Initialize the database
 if __name__ == '__main__':
+    db.create_all()
     serve(app, host="0.0.0.0", port=8080)
